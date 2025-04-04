2e621d0df4f6200fe78d61261c8a7c157fb8bcee
William <wmclaughlan@gmail.com>
2025-04-05 00:06:13 +0100

    pandas testing

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the CSV file
+df = pd.read_csv('animal_data.csv', header=0)
+
+# Display the first few rows of the DataFrame
+#print(df.head())
+
+# Filter rows where 'Conservation Status' is 'Endangered'
+endangered_animals = df[df['Conservation Status'] == 'Endangered']
+long_lived = df[df['Average Lifespan (Years)'] > 50] 
+
+# Print the filtered rows
+print(f"Endagered Animal! {endangered_animals}")
+print(f"Long Lived Animal! {long_lived}")
+print(f"Long Lived Animal Name {long_lived['Name']}") #value for long lived dict 
+
